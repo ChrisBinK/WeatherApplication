5e68121e3c1ef432dec4cff161ad7352ebba3cdb
Christelle <christellekalanda@gmail.com>
2019-05-14 19:27:17 +0200

    new api key update

--- models/WeatherOnline.js
+++ models/WeatherOnline.js
@@ -5,7 +5,7 @@
 
 module.exports = {
     base_url: function () {
-        return 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key=3cf77d9299ee42a08c5162233191303&q=';
+        return 'http://api.worldweatheronline.com/premium/v1/weather.ashx?key=85a54468c9e741dbb11171252191405&q=';
     },
     optional_params: function () {
         return '&num_of_days=7&format=json';
